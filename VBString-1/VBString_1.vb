0431f48f4f666428cead63715c12f4f43393b184
David Shockley <dshockle@hotmail.com>
2018-02-17 13:00:47 -0800

    Add files via upload

---
+++ VBString-1/VBString_1.vb
@@ -0,0 +1,269 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Module VBString_1
+
+    Sub Main()
+
+        Console.WriteLine("seeColor")
+            Console.WriteLine(seeColor("redxx") = "red")
+            Console.WriteLine(seeColor("xxred") = "")
+            Console.WriteLine(seeColor("blueTimes") = "blue")
+
+            Console.WriteLine("seeColorLinq")
+            Console.WriteLine(seeColorLinq("redxx") = "red")
+            Console.WriteLine(seeColorLinq("xxred") = "")
+            Console.WriteLine(seeColorLinq("blueTimes") = "blue")
+    
+            Console.WriteLine("frontAgain")
+            Console.WriteLine(frontAgain("edited") = True)
+            Console.WriteLine(frontAgain("edit") = False)
+            Console.WriteLine(frontAgain("ed") = True)
+
+            Console.WriteLine("minCat")
+            Console.WriteLine(minCat("Hello", "Hi") = "loHi")
+            Console.WriteLine(minCat("Hello", "java") = "ellojava")
+            Console.WriteLine(minCat("java", "Hello") = "javaello")
+
+            Console.WriteLine("extraFront")
+            Console.WriteLine(extraFront("Hello", 3) = "HelHelHel")
+            Console.WriteLine(extraFront("ab", 2) = "abab")
+            Console.WriteLine(extraFront("H", 3) = "HHH")
+
+            Console.WriteLine("without2")
+            Console.WriteLine(without2("HelloHe") = "lloHe")
+            Console.WriteLine(without2("HelloHi") = "HelloHi")
+            Console.WriteLine(without2("") = "")
+            Console.WriteLine(without2(null) = "")
+            Console.WriteLine(without2("H") = "H")
+            Console.WriteLine(without2("Hi") = "")
+
+            Console.WriteLine("deFront")
+            Console.WriteLine(deFront("Hello") = "llo")
+            Console.WriteLine(deFront("java") = "va")
+            Console.WriteLine(deFront("away") = "aay")
+
+            Console.WriteLine("startWord")
+            Console.WriteLine(startWord("hippo", "hi") = "hi")
+            Console.WriteLine(startWord("hippo", "xip") = "hip")
+            Console.WriteLine(startWord("hippo", "z") = "h")
+
+            Console.WriteLine("withoutX")
+            Console.WriteLine(withoutX("xHix") = "Hi")
+            Console.WriteLine(withoutX("xHi") = "Hi")
+            Console.WriteLine(withoutX("Hxix") = "Hxi")
+
+            Console.WriteLine("withoutX2")
+            Console.WriteLine(withoutX2("xHi") = "Hi")
+            Console.WriteLine(withoutX2("Hxi") = "Hi")
+            Console.WriteLine(withoutX2("Hi") = "Hi")
+
+        
+
+        End Function
+        Given a String, If the String begins With "red" Or "blue" Return that color String,
+        otherwise return the empty string. 
+
+        seeColor("redxx") → "red"
+        seeColor("xxred") → ""
+        seeColor("blueTimes") → "blue"
+        
+        Public Function String seeColor(String str)
+        
+            var values = Enum.GetNames(TypeOf(Colors))
+            foreach(String color In values)
+            
+                If (Str.Trim().ToLower().StartsWith(color)) Then
+            Return color
+            
+            Return ""
+        
+        Private Enum Colors  red, green, blue, yellow 
+
+        End Function
+        Given a String, If the String begins With "red" Or "blue" Return that color String, 
+        otherwise return the empty string. Use Linq. Do Not use loops.
+
+        seeColorLinq("redxx") → "red"
+        seeColorLinq("xxred") → ""
+        seeColorLinq("blueTimes") → "blue"
+        
+        Public Function String seeColorLinq(String str)
+        
+            var values = Enum.GetNames(TypeOf(Colors))
+            Return values.Where(value >= str.Trim().ToLower().StartsWith(value))
+    .DefaultIfEmpty(String.Empty).First()
+        
+
+        End Function
+        Given a String, Return True If the first 2 chars In the String also appear at 
+        the end of the string, such as with "edited". 
+
+        frontAgain("edited") → true
+        frontAgain("edit") → false
+        frontAgain("ed") → true
+        
+        Public Function Boolean frontAgain(String str)
+        
+            Return str.Substring(0, 2)
+    .Equals(str.Substring(str.Length - 2),
+        StringComparison.CurrentCultureIgnoreCase)
+        
+
+        End Function
+            Given two strings, append them together And Return the result. 
+            However, if the strings are different lengths, omit chars 
+            from the longer String so it Is the same length As the 
+            shorter string. So "Hello" And "Hi" yield "loHi". 
+            The strings may be any length including empty. 
+
+            minCat("Hello", "Hi") → "loHi"
+            minCat("Hello", "java") → "ellojava"
+            minCat("java", "Hello") → "javaello"        
+        
+        Public Function String minCat(String a, String b)
+        
+            If (a.Length > b.Length)
+    Return a.Substring(a.Length - b.Length) + b
+            ElseIf (a.Length < b.Length)
+    Return a + b.Substring(b.Length - a.Length)
+            Else
+    Return a + b
+        
+
+        End Function
+        Given a String, Return a New String made Of n copies Of the first n chars. 
+        If there are fewer than n chars, use whatever Is there. 
+
+        extraFront("Hello", 3) → "HelHelHel"
+        extraFront("ab", 2) → "abab"
+        extraFront("H", 3) → "HHH"        
+        
+        Public Function String extraFront(String str, int n)
+        
+            String item = str.Substring(0, Math.Min(n, str.Length))
+            Return String.Concat(Enumerable.Repeat(item, n))
+        
+
+        End Function
+
+        Given a String, If a length 2 substring appears at both its beginning 
+        And end, return a string without the substring at the beginning, 
+        so "HelloHe" yields "lloHe". The substring may overlap with itself, 
+        so "Hi" yields "". Otherwise, return the original string unchanged. 
+
+        without2("HelloHe") → "lloHe"
+        without2("HelloHi") → "HelloHi"
+        without2("Hi") → ""        
+        
+        Public Function String without2(String str)
+        
+            If (String.IsNullOrEmpty(str))
+    Return ""
+            ElseIf (str.Length < 2)
+    Return str
+            ElseIf (str.Substring(0, 2).Equals(str.Substring(str.Length - 2),
+                StringComparison.CurrentCultureIgnoreCase))
+    Return str.Substring(2)
+            Else
+    Return str
+        
+
+        End Function
+            Given a String, Return a version without the first 2 chars. 
+            Except keep the first Char If it Is 'a' and keep the second 
+            Char If it Is 'b'. The string may be any length. Harder than it looks. 
+
+            deFront("Hello") → "llo"
+            deFront("java") → "va"
+            deFront("away") → "aay"        
+        
+        Public Function String deFront(String str)
+        
+            Dim sb As New StringBuilder
+            If (str.Length > 0 And str[0] = 'a')
+                sb.Append(str[0])
+            If (str.Length > 1 And str[1] = 'b')
+                sb.Append(str[1])
+            If (str.Length > 2)
+                sb.Append(str.Substring(2))
+            Return sb.ToString()
+        
+
+        End Function
+            Given a String And a second "word" String, we'll say that the word matches 
+            the string if it appears at the front of the string, except its first char 
+            does Not need to match exactly. On a match, return the front of the string, 
+            Or otherwise return the empty string. So, so with the string "hippo" 
+            the word "hi" returns "hi" And "xip" returns "hip". The word will be at 
+            least length 1. 
+
+            startWord("hippo", "hi") → "hi"
+            startWord("hippo", "xip") → "hip"
+            startWord("hippo", "z") → "h"        
+        
+        Public Function String startWord(String str, String word)
+        
+            If (str.Substring(1, word.Length - 1).Equals(word.Substring(1)))
+    Return str.Substring(0, word.Length)
+            Else
+    Return ""
+        
+
+        End Function
+            Given a String, If the first Or last chars are 'x', return the string 
+            without those 'x' chars, and otherwise return the string unchanged. 
+
+            withoutX("xHix") → "Hi"
+            withoutX("xHi") → "Hi"
+            withoutX("Hxix") → "Hxi"        
+        
+        Public Function String withoutX(String str)
+        
+            Dim sb As New StringBuilder
+
+            If (str.Length > 0 And str[0] != 'x')
+                sb.Append(str[0])
+
+            If (str.Length > 2)
+                sb.Append(str.Substring(1, str.Length - 2))
+
+            If (str.Length > 1 And str[str.Length - 1] != 'x')
+                sb.Append(str[str.Length - 1])
+
+            Return sb.ToString()
+        
+
+        End Function
+            Given a String, If one Or both Of the first 2 chars Is 'x', return 
+            the string without those 'x' chars, and otherwise return the string 
+            unchanged. This Is a little harder than it looks. 
+
+            withoutX2("xHi") → "Hi"
+            withoutX2("Hxi") → "Hi"
+            withoutX2("Hi") → "Hi"        
+        
+        Public Function String withoutX2(String str)
+        
+            Dim sb As New StringBuilder
+
+            If (str.Length > 0 And str[0] != 'x')
+                sb.Append(str[0])
+
+            If (str.Length > 1 And str[1] != 'x')
+                sb.Append(str[1])
+
+            If (str.Length > 2)
+                sb.Append(str.Substring(2))
+
+            Return sb.ToString()
+        
+    
+
+
+    End Sub
+
+End Module
---
+++ VBWarmup-2/VBWarmup_2.vb
@@ -0,0 +1,311 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.Threading.Tasks
+Module VBWarmup_2
+
+    Sub Main()
+        Console.WriteLine("stringTimes")
+        Console.WriteLine(stringTimes("Hi", 2) = "HiHi")
+        Console.WriteLine(stringTimes("Hi", 3) = "HiHiHi")
+        Console.WriteLine(stringTimes("Hi", 1) = "Hi")
+
+        Console.WriteLine("stringTimesLinq")
+        Console.WriteLine(stringTimesLinq("Hi", 2) = "HiHi")
+        Console.WriteLine(stringTimesLinq("Hi", 3) = "HiHiHi")
+        Console.WriteLine(stringTimesLinq("Hi", 1) = "Hi")
+
+        Console.WriteLine("frontTimes")
+        Console.WriteLine(frontTimes("Chocolate", 2) = "ChoCho")
+        Console.WriteLine(frontTimes("Chocolate", 3) = "ChoChoCho")
+        Console.WriteLine(frontTimes("Abc", 3) = "AbcAbcAbc")
+
+        Console.WriteLine("frontTimesLinq")
+        Console.WriteLine(frontTimesLinq("Chocolate", 2) = "ChoCho")
+        Console.WriteLine(frontTimesLinq("Chocolate", 3) = "ChoChoCho")
+        Console.WriteLine(frontTimesLinq("Abc", 3) = "AbcAbcAbc")
+
+        Console.WriteLine("countXX")
+        Console.WriteLine(countXX("abcxx") = 1)
+        Console.WriteLine(countXX("xxx") = 2)
+        Console.WriteLine(countXX("xxxx") = 3)
+
+        Console.WriteLine("countXXLinq")
+        Console.WriteLine(countXXLinq("abcxx") = 1)
+        Console.WriteLine(countXXLinq("xxx") = 2)
+        Console.WriteLine(countXXLinq("xxxx") = 3)
+
+        Console.WriteLine("doubleX")
+        Console.WriteLine(doubleX("axXbb") = True)
+        Console.WriteLine(doubleX("axaxx") = False)
+        Console.WriteLine(doubleX("Xxxxx") = True)
+
+        Console.WriteLine("stringBits")
+        Console.WriteLine(stringBits("Hello") = "Hlo")
+        Console.WriteLine(stringBits("Hi") = "H")
+        Console.WriteLine(stringBits("Heeololeo") = "Hello")
+
+        Console.WriteLine("stringSplosion")
+        Console.WriteLine(stringSplosion("Code") = "CCoCodCode")
+        Console.WriteLine(stringSplosion("abc") = "aababc")
+        Console.WriteLine(stringSplosion("ab") = "aab")
+
+        Console.WriteLine("stringX")
+        Console.WriteLine(stringX("xxHxix") = "xHix")
+        Console.WriteLine(stringX("abxxxcd") = "abcd")
+        Console.WriteLine(stringX("xabxxxcdx") = "xabcdx")
+
+        Console.WriteLine("altPairs")
+        Console.WriteLine(altPairs("kitten") = "kien")
+        Console.WriteLine(altPairs("Chocolate") = "Chole")
+        Console.WriteLine(altPairs("CodingHorror") = "Congrr")
+
+        Console.WriteLine("stringYak")
+        Console.WriteLine(stringYak("yakpak") = "pak")
+        Console.WriteLine(stringYak("pakyak") = "pak")
+        Console.WriteLine(stringYak("yak123ya") = "123ya")
+
+        Console.WriteLine("noTriples")
+        Console.WriteLine(noTriples(1, 1, 2, 2, 1) = True)
+        Console.WriteLine(noTriples(1, 1, 2, 2, 2, 1) = False)
+        Console.WriteLine(noTriples(1, 1, 1, 2, 2, 2, 1) = False)
+
+        Console.WriteLine("noTriplesLinq")
+        Console.WriteLine(noTriplesLinq(1, 1, 2, 2, 1) = True)
+        Console.WriteLine(noTriplesLinq(1, 1, 2, 2, 2, 1) = False)
+        Console.WriteLine(noTriplesLinq(1, 1, 1, 2, 2, 2, 1) = False)
+
+        Console.ReadKey()
+
+    End Sub
+
+
+    'Given a String And a non-negative int n, Return a larger String 
+    'that Is n copies of the original string. 
+    'stringTimes("Hi", 2) → "HiHi"
+    'stringTimes("Hi", 3) → "HiHiHi"
+    'stringTimes("Hi", 1) → "Hi"
+
+    Public Function stringTimes(str As String, n As Integer) As String
+        Dim sb As New StringBuilder
+
+        For i As Integer = 1 To n
+            sb.Append(str)
+        Next
+
+        Return sb.ToString()
+
+    End Function
+
+    'Given a String And a non-negative int n, Return a larger String 
+    'that Is n copies of the original string. Use Linq. Do Not use loops.
+    'stringTimesLinq("Hi", 2) → "HiHi"
+    'stringTimesLinq("Hi", 3) → "HiHiHi"
+    'stringTimesLinq("Hi", 1) → "Hi"
+
+    Public Function stringTimesLinq(str As String, n As Integer) As String
+
+        Return String.Concat(Enumerable.Repeat(str, n))
+
+    End Function
+
+    'Given a String And a non-negative int n, 
+    'we'll say that the front of the string is the first 3 chars, 
+    'Or whatever Is there if the string Is less than length 3. 
+    'Return n copies Of the front 
+
+    'frontTimes("Chocolate", 2) → "ChoCho"
+    'frontTimes("Chocolate", 3) → "ChoChoCho"
+    'frontTimes("Abc", 3) → "AbcAbcAbc"
+
+    Public Function frontTimes(str As String, n As Integer) As String
+
+        Dim sb As New StringBuilder
+        Dim front As String = str.Substring(0, Math.Min(str.Length, 3))
+
+        For i As Integer = 1 To n
+            sb.Append(front)
+        Next
+        Return sb.ToString()
+
+    End Function
+
+    'Given a String And a non-negative int n, 
+    'we'll say that the front of the string is the first 3 chars, 
+    'Or whatever Is there if the string Is less than length 3. 
+    'Return n copies Of the front. Use Linq. Do Not use loops.
+
+    'frontTimesLinq("Chocolate", 2) → "ChoCho"
+    'frontTimesLinq("Chocolate", 3) → "ChoChoCho"
+    'frontTimesLinq("Abc", 3) → "AbcAbcAbc"
+
+    Public Function frontTimesLinq(str As String, n As Integer) As String
+
+        Return String.Concat(Enumerable.Repeat(str.Substring(0, 3), n))
+
+    End Function
+
+    'Count the number Of "xx" In the given String. 
+    'We'll say that overlapping is allowed, so "xxx" contains 2. 
+    'countXX("abcxx") → 1
+    'countXX("xxx") → 2
+    'countXX("xxxx") → 3
+
+    Public Function countXX(str As String) As Integer
+
+        Dim count As Integer = 0
+
+        For i As Integer = 1 To str.Length - 1
+            If str(i) = CChar("x") And str(i - 1) = "x" Then
+                count += 1
+            End If
+        Next
+        Return count
+
+    End Function
+
+    'Count the number Of "xx" In the given String. 
+    'We'll say that overlapping is allowed, so "xxx" contains 2. 
+    'Use Linq. Do Not use loops.
+    'countXXLinq("abcxx") → 1
+    'countXXLinq("xxx") → 2
+    'countXXLinq("xxxx") → 3
+
+    Public Function countXXLinq(str As String) As Integer
+        Dim result As Integer = str.Where((Value) >= Value = CChar("x")).Count()
+        Return result
+    End Function
+
+    'Given a String, Return True If the first instance Of "x" In the String 
+    'Is immediately followed by another "x". 
+    'doubleX("axxbb") → true
+    'doubleX("axaxx") → false
+    'doubleX("xxxxx") → true
+
+    Public Function doubleX(str As String) As Boolean
+
+        Dim lowerCaseStr As String = str.ToLower()
+        Dim pos As Integer = lowerCaseStr.IndexOf("x")
+        Return pos > -1 And pos < str.Length - 1 And lowerCaseStr(pos + 1) = CChar("x")
+
+    End Function
+
+    'Given a String, Return a New String made Of every other Char starting 
+    'With the first, so "Hello" yields "Hlo". 
+
+    'stringBits("Hello") → "Hlo"
+    'stringBits("Hi") → "H"
+    'stringBits("Heeololeo") → "Hello"
+
+
+    Public Function stringBits(str As String) As String
+
+        Dim sb As New StringBuilder
+        For i As Integer = 0 To str.Length - 1 Step 2
+            sb.Append(str(i))
+        Next
+        Return sb.ToString()
+
+    End Function
+
+    'Given a non-empty String Like "Code" Return a String Like "CCoCodCode". 
+
+    'stringSplosion("Code") → "CCoCodCode"
+    'stringSplosion("abc") → "aababc"
+    'stringSplosion("ab") → "aab"
+
+    Public Function stringSplosion(str As String) As String
+
+        Dim sb As New StringBuilder
+        For i As Integer = 0 To str.Length - 1
+
+            sb.Append(str.Substring(0, i + 1))
+        Next
+        Return sb.ToString()
+
+    End Function
+
+    'Given a String, Return a version where all the "x" have been removed. 
+    'Except an "x" at the very start Or End should Not be removed. 
+
+    'stringX("xxHxix") → "xHix"
+    'stringX("abxxxcd") → "abcd"
+    'stringX("xabxxxcdx") → "xabcdx"
+
+    Public Function stringX(str As String) As String
+        Dim noX As String = Regex.Replace(str.Substring(1, str.Length - 2), "x", "", RegexOptions.IgnoreCase)
+        Return str(0) + noX + str(str.Length - 1)
+
+    End Function
+
+    'Given a String, Return a String made Of the chars at 
+    'indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien". 
+
+    'altPairs("kitten") → "kien"
+    'altPairs("Chocolate") → "Chole"
+    'altPairs("CodingHorror") → "Congrr
+
+    Public Function altPairs(str As String) As String
+        Dim sb As New StringBuilder
+
+        For i As Integer = 0 To str.Length - 1 Step 4
+            sb.Append(str(i))
+
+            If i < str.Length - 1 Then
+                sb.Append(str(i + 1))
+            End If
+        Next
+
+        Return sb.ToString()
+
+    End Function
+
+    'Suppose the string "yak" is unlucky. 
+    'Given a string, return a version where all the "yak" are removed, 
+    'but the "a" can be any char. The "yak" strings will not overlap. 
+
+    'stringYak("yakpak") → "pak"
+    'stringYak("pakyak") → "pak"
+    'stringYak("yak123ya") → "123ya"
+
+    Public Function stringYak(str As String) As String
+
+        Return System.Text.RegularExpressions.Regex.Replace(str, "y.k", "")
+
+    End Function
+
+
+    'Given an array of ints, we'll say that a triple is a value appearing 3 times 
+    'in a row in the array. Return true if the array does not contain any triples. 
+
+    'noTriples(1, 1, 2, 2, 1) → true
+    'noTriples(1, 1, 2, 2, 2, 1) → false
+    'noTriples(1, 1, 1, 2, 2, 2, 1) → false
+
+    Public Function noTriples(nums() As Integer) As Boolean
+
+        For i As Integer = 2 To nums.Length - 1
+            If (nums(i) = nums(i - 1) And nums(i) = nums(i - 2)) Then
+                Return False
+            End If
+        Next
+
+        Return True
+    End Function
+
+
+
+    'Given an array of ints, we'll say that a triple is a value appearing 3 times 
+    'in a row in the array. Return true if the array does not contain any triples. 
+    'Use Linq. Do not use loops.
+    'noTriplesLinq(1, 1, 2, 2, 1) → true
+    'noTriplesLinq(1, 1, 2, 2, 2, 1) → false
+    'noTriplesLinq(1, 1, 1, 2, 2, 2, 1) → false
+
+    Public Function noTriplesLinq(nums() As Integer) As Boolean
+        Return 0
+    End Function
+
+End Module
